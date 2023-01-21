f4893ef6c07c28245335e626a11c81053a6e1ce7
David Fernández <Dafs@localhost.localdomain>
2023-01-21 00:11:38 -0300

    editing work and contact pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "380a8642cf3b7d59"; }
+/******/ 		__webpack_require__.h = function() { return "85738d774086da05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
